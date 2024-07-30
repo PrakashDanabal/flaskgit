44ee075cb410c577872a02604d61335bec602be1
Prakash <96237649+PrakashDanabal@users.noreply.github.com>
2024-07-30 16:00:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from my Flask website on Google Compute Engine!!!!!!!!!!!!!!!!!"
+def hello():
+    return "Hello from Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,port=int(os.environ.get('PORT',8080)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
